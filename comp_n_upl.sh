2073d7f43b261a47f70a743594a06d2ba3fea6c1
pet <m.a.zhang@outlook.com>
2023-07-14 14:43:30 +0100

    added error checks

--- comp_n_upl.sh
+++ comp_n_upl.sh
@@ -2,6 +2,26 @@
 
 set -eu
 
+# notify if more than one argument is passed
+if [ "$#" -ne 1 ]; then
+  echo "> Usage: $0 DIRECTORY, do not pass more than one directory." >&2
+  exit 1
+else
+    echo "> Start compiling & uploading $1"
+fi
+
+# check if direcory exists
+if ! [ -e "$1" ]; then
+  echo "> $1 not found" >&2
+  exit 1
+fi
+
+# check if argument is a directory
+if ! [ -d "$1" ]; then
+  echo "> $1 not a directory" >&2
+  exit 1
+fi
+
 # compile
 arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328old $1
 
