774f3c7188cde9ad3d6ac25a6d8f4053b1d1cc57
pet <m.a.zhang@outlook.com>
2023-07-18 20:12:15 +0100

    detect whistle above 1000Hz and move forward

--- stream_audio/pi/pi_detect_whistling.py
+++ stream_audio/pi/pi_detect_whistling.py
@@ -22,7 +22,7 @@ def detect_whistling():
                         frames_per_buffer=CHUNK_SIZE)
 
     # Open serial connection to send instructions to the arduino
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    ser = serial.Serial('/dev/ttyUSB0', 19200)
     try:
         ser.open()
     except:
@@ -45,19 +45,40 @@ def detect_whistling():
             fft_r = np.fft.rfft(right)
             freq_r = np.fft.rfftfreq(right.size, 1/RATE)
 
+            # filter frequency
+            """
+            filtered_fft_l = fft_l
+            for i, freq in enumerate(freq_l):
+                if abs(freq) < 500:
+                    filtered_fft_l[i] = 0
+            #filtered_l = np.fft.irfft(filtered_fft_l).real
+            fil_dom_freq = freq_l[np.argmax(filtered_fft_l)]
+            #print(fil_dom_freq)
+            """
+
             # find dominant frequency, i.e., the highest amplitude
-            dom_frequency = freq_l[np.argmax(fft_l)]
+            dom_freq = freq_l[np.argmax(fft_l)]
+            max_amp = np.max(fft_l)
+            print(dom_freq)
 
             # this is the frequency you determined with the recorded audio file
-            target_frequency = 500
+            freq_thre_lower = 1000
+            freq_thre_upper = 5000
 
-            if dom_frequency > target_frequency:
+            if dom_freq > freq_thre_lower:
                 # Send intructions to the robot
-                message = b'o' # turn on the LED               
-                ser.write(message)
+                print("> Dominant frequency bigger than threshold")
+                ser.write(b'o') # turn on the LED
+                ser.write(b'f') # turn on the LED
+            else:
+                # Send intructions to the robot
+                ser.write(b'd') # turn off the LED
+                ser.write(b'x') # stop
 
     except KeyboardInterrupt:
         # Close the stream and socket when interrupted
+        ser.write(b'x') # stop
+        ser.write(b'd') # turn off the LED
         stream.stop_stream()
         stream.close()
         ser.close()
--- stream_audio/react_to_whistle/react_to_whistle.ino
+++ stream_audio/react_to_whistle/react_to_whistle.ino
@@ -1,25 +1,121 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+/*
+React to audio input
+*/
 
-  // init LED pin
-  pinMode(LED_BUILTIN, OUTPUT);
+// includes the "servo" library
+#include <Servo.h>
+// creates two servo objects, one for each motor
+Servo left, right;
+
+// left servo pin
+int left_servo = 8;
+// right servo pin
+int right_servo = 9;
+
+void setup() {
+    // Initialize serial port
+    Serial.begin(19200);
+
+    // init LED pin
+    pinMode(LED_BUILTIN, OUTPUT);
+    // Attach servo pins
+    right.attach(left_servo);
+    left.attach(right_servo);
+
+    // Initialize (no motion)
+    left.write(90);
+    right.write(90);
+
 }
 
 void loop() {
-  // Here you can now write different strategies how to handle and react to serial commands
-   if (Serial.available() > 0) {
-    char newChar = Serial.read();
-    
-    // e.g. if the character that was send is "o", turn LED on
-    if(newChar == 'o') {
-      digitalWrite(LED_BUILTIN, HIGH);
+  
+    // Check for any incoming bytes
+    if (Serial.available() > 0) {
+        char newChar = Serial.read();
+        //String command = Serial.readStringUntil("\n"); //this line cannot be here
+
+        // turn LED on
+        if (newChar == 'o') {
+        //if (command.equalsIgnoreCase("on")) {
+              digitalWrite(LED_BUILTIN, HIGH);
+        }
+
+        // turn LED off
+        if (newChar == 'd') {
+        //if (command.equalsIgnoreCase("off")) {
+              digitalWrite(LED_BUILTIN, LOW);
+        }
+
+        // Respond to command "f"
+        if (newChar == 'f') {
+        //if (command.equalsIgnoreCase("f")) {
+              forward();
+        }
+
+        // Respond to command "b"
+        if (newChar == 'b') {
+        //if (command.equalsIgnoreCase("b")) {
+              backward();
+        }
+
+        // Respond to command "l"
+        if (newChar == 'l') {
+        //if (command.equalsIgnoreCase("l")) {
+              turn_left();
+        }
+
+        // Respond to command "r"
+        if (newChar == 'r') {
+        //if (command.equalsIgnoreCase("r")) {
+              turn_right();
+        }
+
+        // Respond to command "x"
+        if (newChar == 'x') {
+        //if (command.equalsIgnoreCase("x")) {
+              stop();
+        }
+
+        // Wait a bit
+        delay(10);
     }
 
-    // if there's something else sent, turn it off
-    else{
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-    delay(10);
-   }
+    // Wait a bit
+    delay(1);
+}
+
+// Forward
+void forward()
+{
+  left.write(45);
+  right.write(135);
+}
+
+// Backward
+void backward()
+{
+  left.write(135);
+  right.write(45);
+}
+
+// Left
+void turn_left()
+{
+  left.write(45);
+  right.write(45);
+}
+
+// Right
+void turn_right()
+{
+  left.write(135);
+  right.write(135);
+}
+
+// Stop
+void stop()
+{
+  left.write(90);
+  right.write(90);
 }
